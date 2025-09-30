2b35dabb7d71c9768e6e4a556e9f3d138452d6ec
andiedogawa <andimulyanaa91@gmail.com>
2025-09-30 14:32:50 -0500

    Update adsterra.js

--- adsterra.js
+++ adsterra.js
@@ -12,17 +12,3 @@
     s.src = 'https://www.highperformanceformat.com/c38112be63d5116bd3c4f447604955e4/invoke.js';
     document.write(s.outerHTML);
 })();
-// ======= Mobile Banner 320x50 =======
-(function() {
-    var atOptions = {
-        'key' : 'a2587c5abac6fb7c05cd2e91eac1c480',
-        'format' : 'iframe',
-        'height' : 50,
-        'width' : 320,
-        'params' : {}
-    };
-    var s = document.createElement('script');
-    s.type = 'text/javascript';
-    s.src = 'https://www.highperformanceformat.com/a2587c5abac6fb7c05cd2e91eac1c480/invoke.js';
-    document.write(s.outerHTML);
-})();
