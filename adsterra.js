650e9bbd24bdc4ffc0e7ffd275b7a57a649b3d7a
andiedogawa <andimulyanaa91@gmail.com>
2025-09-30 14:58:35 -0500

    Update adsterra.js

--- adsterra.js
+++ adsterra.js
@@ -1,14 +1,8 @@
-// ======= Desktop Banner 728x90 =======
-(function() {
-    var atOptions = {
-        'key' : 'c38112be63d5116bd3c4f447604955e4',
-        'format' : 'iframe',
-        'height' : 90,
-        'width' : 728,
-        'params' : {}
-    };
-    var s = document.createElement('script');
-    s.type = 'text/javascript';
-    s.src = 'https://www.highperformanceformat.com/c38112be63d5116bd3c4f447604955e4/invoke.js';
-    document.write(s.outerHTML);
-})();
+atOptions = {
+    'key' : 'c38112be63d5116bd3c4f447604955e4',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/c38112be63d5116bd3c4f447604955e4/invoke.js"></scr' + 'ipt>');
