b38a4e7c2a90e685bfccef487c266dc5f8cbc1d5
andiedogawa <andimulyanaa91@gmail.com>
2025-09-30 15:30:38 -0500

    Update adsterra.js

--- adsterra.js
+++ adsterra.js
@@ -6,3 +6,12 @@ atOptions = {
     'params' : {}
 };
 document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/c38112be63d5116bd3c4f447604955e4/invoke.js"></scr' + 'ipt>');
+
+atOptions = {
+    'key' : 'a2587c5abac6fb7c05cd2e91eac1c480',
+    'format' : 'iframe',
+    'height' : 50,
+    'width' : 320,
+    'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/a2587c5abac6fb7c05cd2e91eac1c480/invoke.js"></scr' + 'ipt>');
