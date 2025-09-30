9d80a0376a1c0b2a43fada8cf8ca8d1195620d1b
andiedogawa <andimulyanaa91@gmail.com>
2025-09-30 12:56:51 -0500

    Update adsterra.js

--- adsterra.js
+++ adsterra.js
@@ -1,17 +1,29 @@
-atOptions = {
-    'key' : 'c38112be63d5116bd3c4f447604955e4',
-    'format' : 'iframe',
-    'height' : 90,
-    'width' : 728,
-    'params' : {}
-};
-document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/c38112be63d5116bd3c4f447604955e4/invoke.js"></scr' + 'ipt>');
+// ======= Desktop Banner 728x90 =======
+(function() {
+    var atOptions = {
+        'key' : 'c38112be63d5116bd3c4f447604955e4',
+        'format' : 'iframe',
+        'height' : 90,
+        'width' : 728,
+        'params' : {}
+    };
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = 'https://www.highperformanceformat.com/c38112be63d5116bd3c4f447604955e4/invoke.js';
+    document.write(s.outerHTML);
+})();
 
-atOptions = {
-    'key' : 'a2587c5abac6fb7c05cd2e91eac1c480',
-    'format' : 'iframe',
-    'height' : 50,
-    'width' : 320,
-    'params' : {}
-};
-document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/a2587c5abac6fb7c05cd2e91eac1c480/invoke.js"></scr' + 'ipt>');
+// ======= Mobile Banner 320x50 =======
+(function() {
+    var atOptions = {
+        'key' : 'a2587c5abac6fb7c05cd2e91eac1c480',
+        'format' : 'iframe',
+        'height' : 50,
+        'width' : 320,
+        'params' : {}
+    };
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = 'https://www.highperformanceformat.com/a2587c5abac6fb7c05cd2e91eac1c480/invoke.js';
+    document.write(s.outerHTML);
+})();
