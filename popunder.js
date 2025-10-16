b7d63a967493afe99dfc47a1a2a2adb25258d341
andiedogawa <andimulyanaa91@gmail.com>
2025-10-16 14:23:36 -0500

    Create popunder.js

---
+++ popunder.js
@@ -0,0 +1,7 @@
+// Adsterra Popunder Script
+(function() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//pl27864865.effectivegatecpm.com/f3/42/ed/f342edcdcec76e6cfbdf9e5c1f3b56bb.js';
+    document.body.appendChild(script);
+})();
