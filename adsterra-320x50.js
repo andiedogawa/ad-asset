3c6c7be979a134a8fe7227aad63ed0f4275cc7cc
andiedogawa <andimulyanaa91@gmail.com>
2025-10-19 10:55:47 -0500

    Update adsterra-320x50.js

--- adsterra-320x50.js
+++ adsterra-320x50.js
@@ -1,23 +1,27 @@
-// Adsterra 320x50 Auto Inject + Center FIXED
-document.addEventListener("DOMContentLoaded", function () {
-  const adContainer = document.createElement("div");
-  adContainer.style.cssText = `
-    width: 100%;
-    text-align: center;
-    margin: 15px auto;
-  `;
-  document.body.appendChild(adContainer);
+<!-- Iklan 320x50 Pertama -->
+<div style="text-align:center; margin:10px 0;">
+  <script type="text/javascript">
+    atOptions = {
+      'key' : 'a2587c5abac6fb7c05cd2e91eac1c480',
+      'format' : 'iframe',
+      'height' : 50,
+      'width' : 320,
+      'params' : {}
+    };
+  </script>
+  <script type="text/javascript" src="https://www.highperformanceformat.com/a2587c5abac6fb7c05cd2e91eac1c480/invoke.js"></script>
+</div>
 
-  window.atOptions = {
-    'key': 'a2587c5abac6fb7c05cd2e91eac1c480',
-    'format': 'iframe',
-    'height': 50,
-    'width': 320,
-    'params': {}
-  };
-
-  const invoke = document.createElement("script");
-  invoke.type = "text/javascript";
-  invoke.src = "https://www.highperformanceformat.com/a2587c5abac6fb7c05cd2e91eac1c480/invoke.js";
-  adContainer.appendChild(invoke);
-});
+<!-- Iklan 320x50 Kedua -->
+<div style="text-align:center; margin:10px 0;">
+  <script type="text/javascript">
+    atOptions = {
+      'key' : 'a2587c5abac6fb7c05cd2e91eac1c480',
+      'format' : 'iframe',
+      'height' : 50,
+      'width' : 320,
+      'params' : {}
+    };
+  </script>
+  <script type="text/javascript" src="https://www.highperformanceformat.com/a2587c5abac6fb7c05cd2e91eac1c480/invoke.js"></script>
+</div>
