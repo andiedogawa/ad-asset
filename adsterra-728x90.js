2a508506b56b47415546624e948e255c0ef48928
andiedogawa <andimulyanaa91@gmail.com>
2025-10-19 09:17:46 -0500

    Update adsterra-728x90.js

--- adsterra-728x90.js
+++ adsterra-728x90.js
@@ -1,27 +1,23 @@
-// Adsterra Banner 728x90 Auto Center
-document.addEventListener("DOMContentLoaded", function() {
-  // Buat elemen container iklan
+// Adsterra 728x90 Auto Inject + Center FIXED
+document.addEventListener("DOMContentLoaded", function () {
   const adContainer = document.createElement("div");
   adContainer.style.cssText = `
     width: 100%;
     text-align: center;
-    margin: 15px 0;
+    margin: 15px auto;
   `;
-
-  // Tambahkan script Adsterra ke dalam container
-  adContainer.innerHTML = `
-    <script type="text/javascript">
-      atOptions = {
-        'key' : 'c38112be63d5116bd3c4f447604955e4',
-        'format' : 'iframe',
-        'height' : 90,
-        'width' : 728,
-        'params' : {}
-      };
-    <\/script>
-    <script type="text/javascript" src="//www.highperformanceformat.com/c38112be63d5116bd3c4f447604955e4/invoke.js"><\/script>
-  `;
-
-  // Sisipkan di bagian atas body
   document.body.insertBefore(adContainer, document.body.firstChild);
+
+  window.atOptions = {
+    'key': 'c38112be63d5116bd3c4f447604955e4',
+    'format': 'iframe',
+    'height': 90,
+    'width': 728,
+    'params': {}
+  };
+
+  const invoke = document.createElement("script");
+  invoke.type = "text/javascript";
+  invoke.src = "https://www.highperformanceformat.com/c38112be63d5116bd3c4f447604955e4/invoke.js";
+  adContainer.appendChild(invoke);
 });
